89b2c864baa2fea17e7a45ccf775cbc94d342cb5
jcarapet <jeffcarapetyan@gmail.com>
2015-01-26 11:29:30 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'df_nginx'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures df_nginx'
+long_description 'Installs/Configures df_nginx'
+version          '0.1.0'
